98277e83fc43afe02f35dd7d21f97480855b6334
Nathalya Silva <nathalya.gsn@gmail.com>
2025-06-18 11:25:27 -0300

    fim

--- Projeto/script.js
+++ Projeto/script.js
@@ -1,5 +1,13 @@
-if(document.getElementById("user-name").value && document.getElementById("user-email").value !=) { 
-    //
-} else {
-    //
+document.getElementById("enviar").addEventListener("click", validateForm)
+
+function validateForm() {
+    if(document.getElementById("user-name").value != "" &&
+      document.getElementById("user-email").value != "" &&
+      document.getElementById("user-tel").value != "" ){
+
+    alert("Prontinho! você receberá as novidades por email.")
+   }else{
+     alert("Por favor, preencha os campos nome e email.")
+   }
 }
+
